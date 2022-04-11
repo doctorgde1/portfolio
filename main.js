aedde15dab30dc2d4180fcdd1a9a33e3bd5a1b5d
doctorgde1 <96441261+doctorgde1@users.noreply.github.com>
2022-04-11 17:52:17 +0300

    update

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const background_img = document.querySelector('.background-image');
 let i = 0;
+let Orientation = "horizontal";
 
 function call()
 {
-   background_img.setAttribute('src', `images/background_${i}.jpg`);
+   if(window.innerHeight > window.innerWidth)
+   {
+       Orientation = "vertical";
+   }
+   else
+   {
+        Orientation = "horizontal";
+   }
+   background_img.setAttribute('src', `images/${Orientation}/background_${i}.jpg`);
    i++;
-   if(i > 6)
+   if(i > 4)
    {
        i = 0;
    }
