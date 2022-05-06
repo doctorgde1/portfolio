c7cae15cb62772618435c72e4538611c5f8c2a86
doctorgde1 <96441261+doctorgde1@users.noreply.github.com>
2022-05-06 21:51:20 +0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 const background_slider = document.querySelectorAll('.slider__item');
+let i = 0;
+let memory = "horizontal";
 let Orientation = "horizontal";
 
 $(document).ready(function () {
@@ -9,14 +11,14 @@ $(document).ready(function () {
         slidesToShow: 1,
         initialSlide: 0,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 3000,
         pauseOnFocus: false,
         pauseOnHover: false,
         draggable: false,
         swipe: false,
         variableHeight: true,
         fade: true,
-        easing: "ease",
+        easing: "linear",
     });
     $('.background-slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
         if (window.innerHeight > window.innerWidth) {
@@ -25,10 +27,18 @@ $(document).ready(function () {
         else {
             Orientation = "horizontal";
         }
-        background_slider[0].querySelector('img').setAttribute('src', `images/${Orientation}/background_${0}.jpg`);
-        background_slider[1].querySelector('img').setAttribute('src', `images/${Orientation}/background_${1}.jpg`);
-        background_slider[2].querySelector('img').setAttribute('src', `images/${Orientation}/background_${2}.jpg`);
-        background_slider[3].querySelector('img').setAttribute('src', `images/${Orientation}/background_${3}.jpg`);
-        background_slider[4].querySelector('img').setAttribute('src', `images/${Orientation}/background_${4}.jpg`);
+        if (Orientation != memory) {
+            background_slider.forEach(element => {
+                if (element != currentSlide) {
+                    element.querySelector('img').setAttribute('src', `images/${Orientation}/background_${i}.jpg`);
+                    i++;
+                }
+                else {
+                    i++;
+                }
+            });
+            i = 0;
+            memory = Orientation;
+        }
     });
 });
